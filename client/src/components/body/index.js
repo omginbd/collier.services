0e5a0cf0af3af44a8d45ed1fc98960a6e802419a
Michael <mikec@kuali.co>
2018-03-29 18:57:31 -0600

    Flush out better content

--- client/src/components/body/index.js
+++ client/src/components/body/index.js
@@ -10,14 +10,29 @@ export default class Body extends React.Component {
         <div className={style.textInfoWrapper}>
           <div className={style.innerContent}>
             <div className={style.section}>
-              Hi! I'm Michael. I like web apps. I like React. I like node. I
-              like learning new things, but I like mixing it with tried and true
-              tech at the same time. I'm pretty friendly, and I'll get along
-              with a dev manager <span className={style.bold}>and</span> a
-              project/product manager at the same time.
+              <h2>Who Am I?</h2>
+              Hi! I'm Michael. I'm a hard worker, fast learner, active problem
+              solver. Lehi, Utah is where I call home, but I'm sure there are a
+              lot of exciting places around the world! I'm pretty friendly, and
+              I'll get along with a dev manager{' '}
+              <span className={style.bold}>and</span> a project/product manager
+              at the same time.
+            </div>
+            <div className={style.section}>
+              <h2>What do I like?</h2>
+              I like web apps. I like React. I like node. I like learning new
+              things, but I like mixing it with tried and true tech at the same
+              time. I like the quick and dirty, rapid iteration of startup
+              development. I like stable production apps with analytics and
+              monitoring. I like delivering products that make customer's jaws
+              drop. "This will save me <span className={style.bold}>
+                so
+              </span>{' '}
+              much time!" is one of my favorite things to hear.
             </div>
 
             <div className={style.section}>
+              <h2>What have I done?</h2>
               I'm still younger in the lifecycle of my career, but not so young
               that I'm inexperienced. I've worked on some cool things that I'm
               proud of. Some for work, some for fun. Here's a list of those
--- client/src/components/carousel/index.js
+++ client/src/components/carousel/index.js
@@ -4,33 +4,51 @@ import React from 'react'
 import style from './style'
 
 export default class ShowoffCarousel extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      selectedTabIndex: 0
-    }
-  }
-
   render () {
-    const { selectedTabIndex } = this.state
-    const sides = [
-      <div className={style.middle}>side two</div>,
-      <div className={style.left}>side one</div>,
-      <div className={style.right}>side three</div>
-    ]
     return (
       <div className={style.carouselWrapper}>
-        {map(sides, (Side, i) => (
-          <div
-            className={[
-              style.side,
-              selectedTabIndex === i ? style.active : ''
-            ].join(' ')}
-          >
-            {Side}
-          </div>
-        ))}
+        <ShowoffCard link='https://byui.edu/catalog' title='Kuali Catalog'>
+          Kuali makes software that saves people time. I was the sole developer
+          on Kuali Catalog for the year and a half of it's existence. I met with
+          customers, managed and met deadlines and expectations, and coded the
+          app. Catalog provides a turn-key solution for schools that don't want
+          to manage their academic catalog by hand.
+        </ShowoffCard>
+        <ShowoffCard
+          link='http://store.steampowered.com/app/261510/Tesla_Effect_A_Tex_Murphy_Adventure/'
+          title='Tex Murphy'
+        >
+          I was one of four developers who put together a revival of a
+          twenty-year-old cult classic video game on a shoestring, Kickstarted,
+          budget. I also helped manage the Kickstarter campaign, run a
+          teleprompter, and a bunch of other things I'd never expected to be
+          involved in game development.
+        </ShowoffCard>
+        <ShowoffCard link='https://wiffleball.xyz' title='Golfbot'>
+          With the tenth annual Wiffleball Open rapidly approaching, the Poulton
+          Golf Association (PGA) is the premier golf association of Bountiful,
+          Utah. When tallying round totals and maintaining a leaderboard became
+          tedious, I built Golfbot - a system for people to keep track of scores
+          on their phone, updating the leaderboard as the tournament progresses.
+        </ShowoffCard>
       </div>
     )
   }
+}
+
+const ShowoffCard = ({ children, link, title }) => {
+  return (
+    <div className={style.card}>
+      <h3 className={style.cardTitle}>{title}</h3>
+      <div className={style.cardContent}>{children}</div>
+      <div className={style.cardFooter}>
+        <button
+          className={style.cardButton}
+          onClick={() => window.open(link, '_blank')}
+        >
+          Check it out!
+        </button>
+      </div>
+    </div>
+  )
 }
