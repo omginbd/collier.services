e40cd7ddc990d97297744c318eb8b57e3c6c2fcb
Michael <mikec@kuali.co>
2018-03-29 19:15:42 -0600

    style tweaks and typos

--- client/src/components/body/index.js
+++ client/src/components/body/index.js
@@ -10,13 +10,13 @@ export default class Body extends React.Component {
         <div className={style.textInfoWrapper}>
           <div className={style.innerContent}>
             <div className={style.section}>
-              <h2>Who Am I?</h2>
+              <h2>Who am I?</h2>
               Hi! I'm Michael. I'm a hard worker, fast learner, active problem
               solver. Lehi, Utah is where I call home, but I'm sure there are a
               lot of exciting places around the world! I'm pretty friendly, and
-              I'll get along with a dev manager{' '}
-              <span className={style.bold}>and</span> a project/product manager
-              at the same time.
+              I get along with my dev manager{' '}
+              <span className={style.bold}>and</span> my product manager at the
+              same time.
             </div>
             <div className={style.section}>
               <h2>What do I like?</h2>
@@ -24,7 +24,7 @@ export default class Body extends React.Component {
               things, but I like mixing it with tried and true tech at the same
               time. I like the quick and dirty, rapid iteration of startup
               development. I like stable production apps with analytics and
-              monitoring. I like delivering products that make customer's jaws
+              monitoring. I like delivering products that make customers' jaws
               drop. "This will save me <span className={style.bold}>
                 so
               </span>{' '}
@@ -35,8 +35,8 @@ export default class Body extends React.Component {
               <h2>What have I done?</h2>
               I'm still younger in the lifecycle of my career, but not so young
               that I'm inexperienced. I've worked on some cool things that I'm
-              proud of. Some for work, some for fun. Here's a list of those
-              things that I find exceptionally exciting:
+              proud of. Some for work, some for fun. Here's a list of some
+              things that I find to be exceptionally exciting:
             </div>
           </div>
         </div>
