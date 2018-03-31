d22039f207f178e7c787e8285494eb496f24ca76
Michael Collier <mikec@kuali.co>
2018-03-30 18:24:32 -0600

    Color and font changes

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -7,7 +7,10 @@ export default class Header extends React.Component {
   render () {
     return ReactDOM.createPortal(
       <div>
-        <div className={style.colorFlair} />
+        <div className={style.colorFlair}>
+          <a href="https://github.com/omginbd">GITHUB</a>
+          <a href="https://linkedin.com/in/omginbd">LINKEDIN</a>
+        </div>
         <div className={style.title}>Michael Collier</div>
       </div>,
       document.querySelector('header')
